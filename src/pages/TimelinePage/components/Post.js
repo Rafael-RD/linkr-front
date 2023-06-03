1005c44433d1b9ed41b79adc38b9b81b6595c1b6
Rafael <rafaelreinertd@hotmail.com>
2023-06-03 04:12:33 -0300

    feat: likes tooltip style

--- src/pages/TimelinePage/components/Post.js
+++ src/pages/TimelinePage/components/Post.js
@@ -106,7 +106,13 @@ export function Post({ postInfo, myUsername, setReload, disable }) {
                 <img src={picture} alt="profile" />
                 {liked()}
                 <span data-test="counter" data-tooltip-id="likes-tooltip" data-tooltip-content={tooltipContent()} data-tooltip-place="bottom" >{showLikes(likeCount)} likes</span>
-                <Tooltip data-test="tooltip" id="likes-tooltip" />
+                <Tooltip data-test="tooltip" id="likes-tooltip"
+                    style={{
+                        backgroundColor: "rgba(255, 255, 255, 0.9)",
+                        opacity: "1",
+                        color: "#282829",
+                        borderRadius: "17px",
+                    }} />
             </ImgLike>
             <ContentContainer>
                 <NameConfigPost>
@@ -168,6 +174,7 @@ const ImgLike = styled.div`
     svg{
         width: 35px;
         height: 35px;
+        cursor: pointer;
     }
     span{
         font-family: 'Lato', sans-serif;
@@ -175,6 +182,11 @@ const ImgLike = styled.div`
         font-size: 14px;
         text-align: center;
         word-wrap: break-word;
+    }
+    > div{
+        font-family: 'Lato', sans-serif;
+        font-weight: 700;
+        font-size: 13px;
     }
 `;
 
