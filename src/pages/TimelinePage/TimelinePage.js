16fbc56c5d828bcbf94e00796f9c3e0ec7d50bb3
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-06-05 03:48:07 -0300

    Feat: Timeline-Responsivity - Componentização e Responsividade Publish-Post

--- src/pages/TimelinePage/TimelinePage.js
+++ src/pages/TimelinePage/TimelinePage.js
@@ -2,53 +2,14 @@ import styled from "styled-components";
 import Header from "../../components/Header"
 import { useContext, useState } from "react";
 import AuthContext from "../../context/auth.context";
-import axios from "axios";
 import { Timeline } from "./components/Timeline.js";
 import TrendingList from "../../components/TrendingList";
+import { PublishPost } from "./components/PublishPost";
 
 export default function TimelinePage() {
-    const { auth } = useContext(AuthContext);
-    const [disable, setDisable] = useState(false);
     const [reload, setReload] = useState(false);
     const [posts, setPosts] = useState([]);
-    const [form, setForm] = useState({
-        link: "",
-        description: ""
-      });
-
-    function handleChange(e) {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    }
-
-    function postLink(e){
-        e.preventDefault();
-        setDisable(true)
-        if(form.link === "" || form.link === null || form.link === undefined){
-            alert("There was an error publishing your link")
-            setDisable(false)
-        }
-        else{        
-        
-        const config = {
-            headers: { Authorization: `Bearer ${auth.token}` }
-        } 
-        axios.post(`${process.env.REACT_APP_API_URL}/post`, form, config)
-                .then((res) => {
-                    setDisable(false)
-                    //Retorna o Id do Post criado!!!
-                    console.log(res.data)
-                    setForm({link: "", description: ""})
-                    setPosts([{...form, id: res.data.id, userName: auth.username, picture: auth.picture, linkMetadata: null}, ...posts]);
-                    setReload(true);
-                })
-                .catch((err) => {
-                    alert("There was an error publishing your link")
-                    console.log(err.message)
-                    setDisable(false)
-                })
-        }
-    }
-
+    
     return(
         <>
         <Header/>
@@ -57,34 +18,7 @@ export default function TimelinePage() {
                 <h1>timeline</h1>
             <Wrapper>
             <ContentContainer>
-                <PostContent data-test="publish-box">
-                    <img src={auth?.picture}
-                    alt="Imagem do Usuário"
-                    onClick={()=>setForm({link: "https://teste.com", description: `testando um bagulhinho aqui de leve ${(new Date()).toString()} #paz`})}/>
-                    <form onSubmit={postLink}>
-                        <p>What are you going to share today?</p>
-                        <input
-                            data-test="link" 
-                            placeholder="http://..."
-                            name="link"
-                            value={form.link}
-                            disabled={disable}
-                            // required
-                            onChange={handleChange} />
-                        <textarea
-                            data-test="description"
-                            placeholder="Awesome article about #javascript"
-                            name="description"
-                            value={form.description}
-                            disabled={disable}
-                            onChange={handleChange} />
-                            {disable ? (
-                                <button data-test="publish-btn" type="submit" disabled={disable}>Publishing...</button>
-                            ) : (
-                                <button data-test="publish-btn" type="submit" disabled={disable}>Publish</button>
-                            )}
-                    </form>
-                </PostContent>
+                <PublishPost reload={reload} setReload={setReload} posts={posts} setPosts={setPosts}/>
                 <Timeline reload={reload} setReload={setReload} posts={posts} setPosts={setPosts}/>
             </ContentContainer>
             <TrendingList/>
@@ -110,7 +44,17 @@ const TimeLineContainer = styled.div`
         margin-bottom: 43px;
         line-height: 64px;
     }
-
+    @media (max-width: 425px) {
+        /* max-width: 425px; */
+    h1{
+        margin-left: 17px;
+        margin-bottom: 10px;
+        margin-top: 3px;
+        font-size: 33px;
+        line-height: 49px;
+    }
+  }
+   
 `
 
 const ContentContainer = styled.div`
@@ -118,6 +62,7 @@ const ContentContainer = styled.div`
     flex-direction: column;
     width: 611px;
     height: 100%;
+    background-color: lightblue;
     h1{
         font-family: 'Oswald', sans-serif;
         color: white;
@@ -125,69 +70,10 @@ const ContentContainer = styled.div`
         font-size: 43px;
         width: 100%;
     }
-`
-const PostContent = styled.div`
+    @media (max-width: 425px) {
     width: 100%;
-    max-width: 611px;
-    height: 209px;
-    border-radius: 16px;
-    background-color: white;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    padding: 16px;
-    img{
-        width: 50px;
-        height: 50px;
-        object-fit: cover;
-        border-radius: 50%;
-    }
-    form{
-        display: flex;
-        flex-direction: column;
-        position: relative;
-        input, p, textarea{
-            width: 503px;
-            height: 30px;
-            border-radius: 5px;
-            background-color: #EFEFEF;
-            border: none;
-            margin-bottom: 5px;
-            font-family: 'Lato', sans-serif;
-            font-weight: 300;
-            line-height: 18px;
-            padding: 10px;
-        }
-        p {
-            background-color: white;
-            height: 40px;
-            font-size: 20px;
-            color: #707070;
-            line-height: 24px;
-            margin-bottom: 0px;
-        }
-        textarea{
-            display: flex;
-            height: 66px;
-            resize: none;
-        }
-        button{
-            width: 112px;
-            height: 31px;
-            background-color: #1877f2;
-            border-radius: 5px;
-            border: none;
-            position: absolute;
-            bottom: 0px;
-            right: 0px;
-            font-family: 'Lato', sans-serif;
-            font-weight: 700;
-            font-size: 14px;
-            color: white;
-            cursor: pointer;
-
-        }       
-    }
+    max-width: 425px;
+  }
 `
 const Wrapper =styled.div`
   display: flex;
@@ -195,4 +81,6 @@ const Wrapper =styled.div`
 `
 const Container = styled.div`
     margin-top: 53px;
+    background-color: lightcoral;
+
 `
---
+++ src/pages/TimelinePage/components/PublishPost.js
@@ -0,0 +1,179 @@
+import { useContext, useEffect, useState } from "react";
+import styled from "styled-components"
+import axios from "axios";
+import AuthContext from "../../../context/auth.context";
+
+
+export function PublishPost({reload, setReload, posts, setPosts}) {
+    const { auth } = useContext(AuthContext);
+    const [disable, setDisable] = useState(false);
+    const [form, setForm] = useState({
+        link: "",
+        description: ""
+      });
+
+    function handleChange(e) {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    function postLink(e){
+        e.preventDefault();
+        setDisable(true)
+        if(form.link === "" || form.link === null || form.link === undefined){
+            alert("There was an error publishing your link")
+            setDisable(false)
+        }
+        else{        
+        
+        const config = {
+            headers: { Authorization: `Bearer ${auth.token}` }
+        } 
+        axios.post(`${process.env.REACT_APP_API_URL}/post`, form, config)
+                .then((res) => {
+                    setDisable(false)
+                    console.log(res.data)
+                    setForm({link: "", description: ""})
+                    setPosts([{...form, id: res.data.id, userName: auth.username, picture: auth.picture, linkMetadata: null}, ...posts]);
+                    setReload(true);
+                })
+                .catch((err) => {
+                    alert("There was an error publishing your link")
+                    console.log(err.message)
+                    setDisable(false)
+                })
+        }
+    }
+
+    return(
+        <PostContent data-test="publish-box">
+        <img src={auth?.picture}
+        alt="Imagem do Usuário"
+        onClick={()=>setForm({link: "https://teste.com", description: `testando um bagulhinho aqui de leve ${(new Date()).toString()} #paz`})}/>
+        <form onSubmit={postLink}>
+            <p>What are you going to share today?</p>
+            <input
+                data-test="link" 
+                placeholder="http://..."
+                name="link"
+                value={form.link}
+                disabled={disable}
+                // required
+                onChange={handleChange} />
+            <textarea
+                data-test="description"
+                placeholder="Awesome article about #javascript"
+                name="description"
+                value={form.description}
+                disabled={disable}
+                onChange={handleChange} />
+                {disable ? (
+                    <button data-test="publish-btn" type="submit" disabled={disable}>Publishing...</button>
+                ) : (
+                    <button data-test="publish-btn" type="submit" disabled={disable}>Publish</button>
+                )}
+        </form>
+    </PostContent>
+    )
+
+
+}
+const PostContent = styled.div`
+    width: 100%;
+    max-width: 611px;
+    height: 209px;
+    border-radius: 16px;
+    background-color: white;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    padding: 16px;
+    img{
+        width: 50px;
+        height: 50px;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        input, p, textarea{
+            width: 503px;
+            height: 30px;
+            border-radius: 5px;
+            background-color: #EFEFEF;
+            border: none;
+            margin-bottom: 5px;
+            font-family: 'Lato', sans-serif;
+            font-weight: 300;
+            line-height: 18px;
+            padding: 10px;
+        }
+        p {
+            background-color: white;
+            height: 40px;
+            /* vertical-align: top; */
+            font-size: 20px;
+            color: #707070;
+            line-height: 24px;
+            margin-bottom: 0px;
+        }
+        textarea{
+            display: flex;
+            height: 66px;
+            resize: none;
+        }
+        button{
+            width: 112px;
+            height: 31px;
+            background-color: #1877f2;
+            border-radius: 5px;
+            border: none;
+            position: absolute;
+            bottom: 0px;
+            right: 0px;
+            font-family: 'Lato', sans-serif;
+            font-weight: 700;
+            font-size: 14px;
+            color: white;
+            cursor: pointer;
+
+        }       
+    }
+    @media (max-width: 425px) {
+    width: 100%;
+    height: 184px;
+    justify-content: center;
+    padding: 10px 15px 16px 15px;
+    border-radius: 0px;
+    /* align-items: center; */
+    img{
+        display: none;
+    }
+    p{
+        width: 100%;
+        text-align: center;
+        height: 30px;
+        font-size: 17px;
+        
+    }
+    form{
+        width: 100%;
+        input, p, textarea{
+            width: 100%;
+            /* background-color: red; */
+        }
+        button{
+            width: 112px;
+            height: 22px;
+            font-size: 13px;
+        }
+        textarea{
+            height: 52px;
+            resize: none;
+        }
+    }
+    
+    
+  }
+`
