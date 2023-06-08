032943df7b8684786db776a85908d870ce6e319e
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-06-08 15:04:54 -0300

    Fix: Merge Issue

--- src/pages/TimelinePage/components/Timeline.js
+++ src/pages/TimelinePage/components/Timeline.js
@@ -57,8 +57,8 @@ export function Timeline({reload, setReload, posts, setPosts, setLoaded}) {
         } else {
             return (
                 <>
-                    {posts.map(e => <Post key={e.id+e.userName} postInfo={e} myUsername={auth?.username} 
-                    setReload={setReload} disable={disable}/>)}
+                    {posts.map(e => <Post key={e.id+e.repostUserName} postInfo={e} myUsername={auth?.username} 
+                    setReload={setReload} disable={disable} posts={posts} setPosts={setPosts} />)}
                 </>
             )
         }
